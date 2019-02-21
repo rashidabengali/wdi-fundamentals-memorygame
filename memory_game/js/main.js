825f4a8a063e31da470b0dcd23555669f455f847
rashidabengali <bengali.rashida@gmail.com>
2019-02-21 21:59:24 +1100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!")
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("user flipped " + cardOne);
+// console.log("user flipped" + " " + cardTwo);
+console.log("user flipped" + " " + cardThree);
+// console.log("user flipped" + " " + cardFour);
