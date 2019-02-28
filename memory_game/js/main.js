39b5aac36b13883eec3fcfd2574c4345f9749ed2
rashidabengali <bengali.rashida@gmail.com>
2019-03-01 00:48:57 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -13,20 +13,42 @@ console.log("user flipped" + " " + cardFour);
 */
 alert ('Hello, Friends!');
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
-var checkForMatch = function () {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+		alert("You found a match!");
 	} else {
-	alert("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 };
-
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 /*	var cardOne = cards[0];
 cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
@@ -34,24 +56,20 @@ console.log("User flipped " + cardOne);
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);*/
 	if (cardsInPlay.length === 2) {
-	checkForMatch();
+		checkForMatch();
 	/*if (cardsInPlay[0] === cardsInPlay[1]) {
  		alert("You found a match!");
 	 } else {
  		alert("Sorry, try again.");
 	 } */
-	} else {
- 	alert("Choose one more card.");
- 	} 
+	}
  };
-//	flipCard();
 	flipCard(0);
 	flipCard(2);
 /*
 var cardThree = cards[1];
 cardsInPlay.push(cardThree);
 console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
-
 var cardFour = cards[3];
 cardsInPlay.push(cardFour);
 console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
